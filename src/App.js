19d295ec2f14e001e911fb181dcacf88a51faa43
Shoaib <125362241+615shoaib@users.noreply.github.com>
2023-04-12 11:33:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
+
 import Login from './Component/Login';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import AppProvider from './Context/Appprovider';
 
 function App() {
+  const clientId = "376415915784-s671r2hqnif828gb5r253kj7ehfs9t07.apps.googleusercontent.com"
   return (
-    
-    <>
-    <Login />
-    </>
+
+    <GoogleOAuthProvider clientId={clientId}>
+      <AppProvider>
+        <Login />
+      </AppProvider>
+
+    </GoogleOAuthProvider>
   );
 }
 
